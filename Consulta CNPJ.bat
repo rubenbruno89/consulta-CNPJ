43b914ea787490458775a5dc91057c4a608ae27c
Ruben Bruno Cavalcante de Menezes <34634888+rubenbruno89@users.noreply.github.com>
2024-03-14 17:30:04 -0300

    Update Consulta CNPJ.bat

--- Consulta CNPJ.bat
+++ Consulta CNPJ.bat
@@ -5,7 +5,9 @@ echo ==========================
 echo     Consulta de CNPJ
 echo ==========================
 set /p cnpj=Digite o cnpj sem ponto:
-curl "https://www.receitaws.com.br/v1/cnpj/%cnpj%" | findstr /C:"nome" /C:"cnpj" /C:"situacao" /C:"data_situacao" /C:"logradouro" /C:"numero" /C:"municipio" /C:"bairro" /C:"uf" /C:"cep" /C:"telefone"
+curl -s "https://www.receitaws.com.br/v1/cnpj/%cnpj%" > resutado.txt
+type resutado.txt | findstr /C:"nome" /C:"cnpj" /C:"situacao" /C:"data_situacao" /C:"logradouro" /C:"numero" /C:"municipio" /C:"bairro" /C:"uf" /C:"cep" /C:"telefone"
+del resutado.txt
 echo.
 echo PARA SAI APERTE CTRL + C
 goto loop
