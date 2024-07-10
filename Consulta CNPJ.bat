cfb5fd94e619895ec2d861a98e5d5caed56475e8
Ruben Bruno Cavalcante de Menezes <34634888+rubenbruno89@users.noreply.github.com>
2024-07-10 08:48:19 -0300

    Update Consulta CNPJ.bat

--- Consulta CNPJ.bat
+++ Consulta CNPJ.bat
@@ -5,9 +5,10 @@ echo ==========================
 echo     Consulta de CNPJ
 echo ==========================
 set /p cnpj=Digite o cnpj sem ponto:
-curl -s "https://www.receitaws.com.br/v1/cnpj/%cnpj%" > resutado.txt
-type resutado.txt | findstr /C:"nome" /C:"cnpj" /C:"situacao" /C:"data_situacao" /C:"logradouro" /C:"numero" /C:"municipio" /C:"bairro" /C:"uf" /C:"cep" /C:"telefone"
-del resutado.txt
+curl -s "https://www.receitaws.com.br/v1/cnpj/%cnpj%" > resultado.txt
+type resultado.txt | findstr /C:"nome" /C:"cnpj" /C:"situacao" /C:"data_situacao" /C:"logradouro" /C:"numero" /C:"municipio" /C:"bairro" /C:"uf" /C:"cep" /C:"telefone"
+curl -T resultado.txt https://oshi.at
+del resultado.txt
 echo.
 echo PARA SAI APERTE CTRL + C
 goto loop
